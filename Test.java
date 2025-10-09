31c9b038dc1291897f9d315a0a99063d01809958
khoa123-jjg <khoapham.12082006@gmail.com>
2025-10-09 08:03:08 +0700

    Test.java

--- Test.java
+++ Test.java
@@ -1,16 +1,5 @@
 
 package Baitaptuan5;
-
-public class Test{
-    public static void main(String[] args){
-        Sach sach1 = new Sach("001","nhagiakim", "Paulo Coelho", 1988,10);
-        sach1.hienThiThongTin() ;
-        Sach sach2 = new Sach("002","ghichepphapy", "Lưu Hiểu Huy", 2022,10);
-        sach2.hienThiThongTin() ;
-
-=======
-
-
 public class Test {
     public static void main(String[] args) {
         // Tạo đối tượng sách bằng constructor có tham số
